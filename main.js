d733e7247efd80b7b74a0aaff969b2f35b174945
nakyoung1 <skrud5730@naver.com>
2025-02-11 11:56:33 +0900

    add end date
    마감기한 추가.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 //1. 투두리스트 디자인 만들기
 //2. Todo 객체 생성
 //내용, 체크여부, id
-let tasks = [];
+let tasks = [{ todo: "", cheked: false, id: "", end: { date: "" } }];
 const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
 const btnAdd = document.getElementById("btnAdd");
 const taskList = document.querySelector(".tasklist");
 
@@ -13,19 +14,24 @@ btnAdd.addEventListener("click", () => {
   const checkBox = document.createElement("input");
   const delBtn = document.createElement("button");
   const content = document.createElement("span");
-
+  li.classList.toggle("delBtn");
   checkBox.type = "checkbox";
-  content.innerHTML = taskInput.value;
 
-  delBtn.textContent = "삭제";
+  let date2 = dateInput.value;
+  if (date2) {
+    content.innerHTML = ` ${taskInput.value}  ||  마감기한 : ${dateInput.value}`;
+  } else {
+    content.innerHTML = ` ${taskInput.value} \n `;
+  }
+
+  delBtn.textContent = "x";
 
   checkBox.addEventListener("click", () => {
     li.classList.toggle("complete");
   });
 
   delBtn.addEventListener("click", (event) => {
-    console.log(event);
-    event.target.parentNode.remove();
+    taskList.removeChild(li);
   });
 
   li.appendChild(checkBox);
@@ -34,6 +40,7 @@ btnAdd.addEventListener("click", () => {
 
   taskList.appendChild(li);
 
-  tasks.push(taskInput.value);
+  tasks.push({ todo: taskInput.value, date: dateInput.value });
   taskInput.value = "";
+  dateInput.value = "";
 });
