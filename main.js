6e9b8c577cb0453cdbe5b0488c4b57811e59e7f5
nakyoung1 <skrud5730@naver.com>
2025-02-14 16:09:54 +0900

    css

--- main.js
+++ main.js
@@ -1,112 +1,91 @@
 //1. 투두리스트 디자인 만들기
 //2. Todo 객체 생성
 //내용, 체크여부, id
-let tasks = [];
+//할일 목록 저장할 배열
+let tasks = JSON.parse(localStorage.getItem("todos")) || [];
+//필요한 요소 불러옴
+const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const btnAdd = document.getElementById("btnAdd");
 const taskList = document.querySelector(".tasklist");
-const category = document.getElementById("category");
+const categoryInput = document.getElementById("category");
 
-tasks = JSON.parse(localStorage.getItem("todos"));
+// 로컬 스토리지에 저장된 목록 불러오기
+loadData();
 
+//저장된 데이터를 가져오는 함수
 function loadData() {
   if (!tasks) tasks = [];
-  tasks.forEach((item) => {});
+  tasks.forEach((item) => {
+    makeTag(item);
+  });
 }
 
-function makeTag(id, todo, date, category) {
+//화면에 표시할 태그를 만드는 함수
+function makeTag({ id, todo, date, category, completed }) {
+  // li 생성
   const li = document.createElement("li");
+  li.id = id;
+
+  const li2 = document.createElement("li");
+  //체크박스 생성
   const checkBox = document.createElement("input");
-  const delBtn = document.createElement("button");
-  const content = document.createElement("span");
-  const editBtn = document.createElement("button");
-  editBtn.textContent = "edit";
-  delBtn.textContent = "delete";
-  const newId = id;
-  li.setAttribute("id", newId);
   checkBox.type = "checkbox";
+  checkBox.checked = completed;
+  checkBox.class = "checkbox";
+  //할일 내용 표시 span 생성
+  const content = document.createElement("span");
+  content.className = "content";
 
-  //날짜값이 있다면 마감기한을 출력
-  if (dateInput.value) {
-    content.innerHTML = ` ${taskInput.value}  ||  마감기한 : ${dateInput.value} || 카테고리 ${category.value}`;
-  } else {
-    content.innerHTML = ` ${taskInput.value} || 카테고리 ${category.value} `;
-  }
+  const li3 = document.createElement("li");
 
+  const dateText = document.createElement("span");
+  dateText.className = "datetext";
+
+  const categoryText = document.createElement("span");
+  categoryText.className = "categorytext";
+  // 할일 수정버튼 생성
+  const editBtn = document.createElement("button");
+  editBtn.className = "editbtn";
+  editBtn.textContent = "edit";
+  // 할일  삭제버튼 생성
+  const delBtn = document.createElement("button");
+  delBtn.className = "delbtn";
+  delBtn.textContent = "delete";
+
+  content.innerHTML = ` ${todo} `;
+  dateText.innerHTML = `${date ? `마감기한 : ${date}` : ""}`;
+  categoryText.innerHTML = `카테고리 : ${category}`;
   //체크박스에 체크 시 선 추가
   checkBox.addEventListener("change", () => {
-    if (checkBox.checked) {
-      content.style.textDecoration = "line-through";
-    } else {
-      content.style.textDecoration = "none";
-    }
+    //tasks 배열 안의 객체 id 와 li의 id가 같은 객체를 checkCompleted에 할당
+    const checkCompleted = tasks.find((item) => item.id === id);
+    //해당 객체에서 compledted 값(true / false) 확인해서 업데이트
+    checkCompleted.completed = checkBox.checked;
+    saveTasks(); //로컬 스토리지 저장
+    content.style.textDecoration = checkBox.checked ? "line-through" : "none";
   });
 
   //수정 버튼 클릭 시 할일 / 마감기한 수정
-  const editText = document.getElementById("edit-text");
-  const editDate = document.getElementById("edit-date");
-  const dialog = document.querySelector("dialog");
-  const confirmBtn = document.getElementById("confirmBtn");
-  const cancelBtn = document.getElementById("cancelBtn");
-  editText.placeholder = "수정 할 일을 입력하세요.";
-  let editTask;
-
-  editBtn.onclick = (event) => {
-    editTask = content;
-    console.log(event);
-    const todoID = event.target.parentNode.getAttribute("id");
-    dialog.show();
-
-    confirmBtn.onclick = () => {
-      if (!editText.value && !editDate.value) {
-        dialog.close();
-        return;
-      }
-      if (!editText.value && editDate.value) {
-        editTask.innerHTML = ` ${editText.value}  ||  마감기한 : ${editDate.value} || 카테고리 : ${category.value}`;
-      } else {
-        editTask.innerHTML = ` ${editText.value}  || 카테고리 : ${category.value}`;
-      }
-      if (editDate.value) {
-        editTask.innerHTML = ` ${editText.value}  ||  마감기한 : ${editDate.value} || 카테고리 : ${category.value}`;
-      } else {
-        editTask.innerHTML = ` ${editText.value}  || 카테고리 : ${category.value}`;
-      }
-      const updateTodo = tasks.find((list) => Number(list.id) === Number(todoID));
-
-      updateTodo.todo = editText.value;
-      updateTodo.date = editDate.value;
-      localStorage.setItem("todos", JSON.stringify(tasks));
-      dialog.close();
-    };
+  editBtn.onclick = () => {
+    openEditDialog(id, content, dateText, categoryText);
   };
-  cancelBtn.onclick = () => {
-    dialog.close();
+
+  delBtn.onclick = () => {
+    deleteTask(id, li);
   };
-  editText.value = "";
-  editDate.value = "";
-  // 삭제 버튼 클릭 시 리스트에서 삭제
-  delBtn.addEventListener("click", (event) => {
-    taskList.removeChild(li);
-    const todoID = event.target.parentNode.getAttribute("id");
-    const delTodo = tasks.find((list) => Number(list.id) === Number(todoID));
-    console.log(todoID);
-    const newTasks = tasks.filter((item) => item.id !== delTodo.id);
-    tasks = newTasks;
-    console.log(newTasks);
-    console.log(tasks);
-  });
-  // input 값을 tasks 배열에 추가
-  tasks.push({ id: newId, todo: taskInput.value, date: dateInput.value });
-  localStorage.setItem("todos", JSON.stringify(tasks));
-  //입력값을 초기화
-  taskInput.value = "";
-  dateInput.value = "";
+
   //요소를 li에 추가
-  li.appendChild(checkBox);
-  li.appendChild(content);
-  li.appendChild(editBtn);
-  li.appendChild(delBtn);
+  li2.appendChild(checkBox);
+  li2.appendChild(content);
+  li2.appendChild(dateText);
+
+  li3.appendChild(categoryText);
+  li3.appendChild(editBtn);
+  li3.appendChild(delBtn);
+
+  li.appendChild(li2);
+  li.appendChild(li3);
 
   //ul인 taskList에 li추가
   taskList.appendChild(li);
@@ -116,100 +95,59 @@ function makeTag(id, todo, date, category) {
 btnAdd.addEventListener("click", () => {
   //유효하지 않다면 아래 코드는 실행하지 않음
   if (!taskInput.value) return;
+  const newTask = {
+    id: Date.now(),
+    todo: taskInput.value,
+    date: dateInput.value,
+    category: categoryInput.value,
+    completed: false, // false인 값이 체크되지 않은 값으로 설정
+  };
 
-  const li = document.createElement("li");
-  const checkBox = document.createElement("input");
-  const delBtn = document.createElement("button");
-  const content = document.createElement("span");
-  const editBtn = document.createElement("button");
-  editBtn.textContent = "edit";
-  delBtn.textContent = "delete";
-  const newId = Date.now();
-  li.setAttribute("id", newId);
-  checkBox.type = "checkbox";
+  tasks.push(newTask);
+  saveTasks();
+  makeTag(newTask);
 
-  //날짜값이 있다면 마감기한을 출력
-  if (dateInput.value) {
-    content.innerHTML = ` ${taskInput.value}  ||  마감기한 : ${dateInput.value} || 카테고리 ${category.value}`;
-  } else {
-    content.innerHTML = ` ${taskInput.value} || 카테고리 ${category.value} `;
-  }
+  //입력값을 초기화
+  taskInput.value = "";
+  dateInput.value = "";
+});
 
-  //체크박스에 체크 시 선 추가
-  checkBox.addEventListener("change", () => {
-    if (checkBox.checked) {
-      content.style.textDecoration = "line-through";
-    } else {
-      content.style.textDecoration = "none";
-    }
-  });
-
-  //수정 버튼 클릭 시 할일 / 마감기한 수정
+function openEditDialog(id, content, dateText, categoryText) {
+  const dialog = document.querySelector("dialog");
   const editText = document.getElementById("edit-text");
   const editDate = document.getElementById("edit-date");
-  const dialog = document.querySelector("dialog");
   const confirmBtn = document.getElementById("confirmBtn");
   const cancelBtn = document.getElementById("cancelBtn");
-  editText.placeholder = "수정 할 일을 입력하세요.";
-  let editTask;
 
-  editBtn.onclick = (event) => {
-    editTask = content;
-    console.log(event);
-    const todoID = event.target.parentNode.getAttribute("id");
-    dialog.show();
+  const task = tasks.find((item) => item.id === id);
+  editText.value = task.todo;
+  editDate.value = task.date;
 
-    confirmBtn.onclick = () => {
-      if (!editText.value && !editDate.value) {
-        dialog.close();
-        return;
-      }
-      if (!editText.value && editDate.value) {
-        editTask.innerHTML = ` ${editText.value}  ||  마감기한 : ${editDate.value} || 카테고리 : ${category.value}`;
-      } else {
-        editTask.innerHTML = ` ${editText.value}  || 카테고리 : ${category.value}`;
-      }
-      if (editDate.value) {
-        editTask.innerHTML = ` ${editText.value}  ||  마감기한 : ${editDate.value} || 카테고리 : ${category.value}`;
-      } else {
-        editTask.innerHTML = ` ${editText.value}  || 카테고리 : ${category.value}`;
-      }
-      const updateTodo = tasks.find((list) => Number(list.id) === Number(todoID));
+  dialog.show();
+  confirmBtn.onclick = () => {
+    task.todo = editText.value || task.todo;
+    task.date = editDate.value || task.date;
+    saveTasks();
+    content.innerHTML = ` ${task.todo} `;
+    dateText.innerHTML = `${task.date ? `마감기한 : ${task.date}` : ""}`;
+    categoryText.innerHTML = `카테고리 : ${categoryInput}`;
 
-      updateTodo.todo = editText.value;
-      updateTodo.date = editDate.value;
-      localStorage.setItem("todos", JSON.stringify(tasks));
-      dialog.close();
-    };
-  };
-  cancelBtn.onclick = () => {
     dialog.close();
   };
   editText.value = "";
   editDate.value = "";
-  // 삭제 버튼 클릭 시 리스트에서 삭제
-  delBtn.addEventListener("click", (event) => {
-    taskList.removeChild(li);
-    // const todoID = event.target.parentNode.getAttribute("id");
-    // const delTodo = tasks.find((list) => Number(list.id) === Number(todoID));
-    // console.log(todoID);
-    // const newTasks = tasks.filter((item) => item.id !== delTodo.id);
-    // tasks = newTasks;
-    // console.log(newTasks);
-    // console.log(tasks);
-  });
-  // input 값을 tasks 배열에 추가
-  tasks.push({ id: newId, todo: taskInput.value, date: dateInput.value });
-  localStorage.setItem("todos", JSON.stringify(tasks));
-  //입력값을 초기화
-  taskInput.value = "";
-  dateInput.value = "";
-  //요소를 li에 추가
-  li.appendChild(checkBox);
-  li.appendChild(content);
-  li.appendChild(editBtn);
-  li.appendChild(delBtn);
 
-  //ul인 taskList에 li추가
-  taskList.appendChild(li);
-});
+  cancelBtn.onclick = () => {
+    dialog.close();
+  };
+}
+
+function deleteTask(id, li) {
+  tasks = tasks.filter((item) => item.id !== id);
+  saveTasks();
+  taskList.removeChild(li);
+}
+
+function saveTasks() {
+  localStorage.setItem("todos", JSON.stringify(tasks));
+}
